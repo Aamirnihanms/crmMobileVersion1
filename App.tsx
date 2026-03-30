4c42def1f4c7ce85e815bc4ba0feec28a84740e8
Aamirnihanms <aamirnihan200@gmail.com>
2026-03-30 14:19:40 +0530

    resolved safearea

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {
   NavigationContainer,
 } from '@react-navigation/native';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { StatusBar } from 'expo-status-bar';
 
 import { queryClient } from './src/lib/queryClient';
 import RootNavigator from './src/navigation/RootNavigator';
@@ -30,6 +31,11 @@ export default function App() {
     <KeyboardProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <SafeAreaProvider>
+          <StatusBar
+            style="dark"
+            backgroundColor={colors.background}
+            translucent
+          />
           <QueryClientProvider client={queryClient}>
             <NavigationContainer theme={appTheme}>
               <RootNavigator />
